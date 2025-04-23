c3b8ee5d39775a72443e5d3fb30303230dcf4c9b
Abdullah <abdullahvora136@gmail.com>
2025-04-23 14:29:22 +0530

    deploy

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+
 const apiInstance = axios.create({
   // baseURL: "https://web-defender-backend.vercel.app",
-  baseURL: "http://127.0.0.1:8002",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
